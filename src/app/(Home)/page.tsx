5857530d4eeb42dd99ff977c843b3f5016cf8dcc
hyeonw8 <whgusrud223@gmail.com>
2024-07-04 15:03:59 +0900

    🚚 move : main 폴더 -> Home 폴더로 변경 및 page.tsx 이동

---
+++ src/app/(Home)/page.tsx
@@ -0,0 +1,10 @@
+// import PokemonList from '@/app/pokemons/_components/PokemonList';
+import PokemonContainer from '../pokemons/_components/PokemonContainer';
+
+export default function Home() {
+  return (
+    <div>
+      <PokemonContainer />
+    </div>
+  );
+}
