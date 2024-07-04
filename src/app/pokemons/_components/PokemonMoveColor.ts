3915be01063d608913afeecd7da545e726bb46aa
hyeonw8 <whgusrud223@gmail.com>
2024-07-04 15:07:06 +0900

    🎉 add : 포켓몬 타입 특성별 배경 색 지정 파일 생성

---
+++ src/app/pokemons/_components/PokemonMoveColor.ts
@@ -0,0 +1,41 @@
+export type Colors = {
+  노말: string;
+  불꽃: string;
+  물: string;
+  풀: string;
+  전기: string;
+  얼음: string;
+  격투: string;
+  독: string;
+  땅: string;
+  비행: string;
+  에스퍼: string;
+  벌레: string;
+  바위: string;
+  고스트: string;
+  드래곤: string;
+  악: string;
+  강철: string;
+  페어리: string;
+};
+
+export const typeColors: Colors = {
+  노말: 'bg-zinc-400',
+  불꽃: 'bg-red-500',
+  물: 'bg-sky-300',
+  풀: 'bg-green-400',
+  전기: 'bg-yellow-300',
+  얼음: 'bg-blue-300',
+  격투: 'bg-orange-400',
+  독: 'bg-fuchsia-600',
+  땅: 'bg-amber-600',
+  비행: 'bg-blue-500',
+  에스퍼: 'bg-rose-400',
+  벌레: 'bg-lime-600',
+  바위: 'bg-stone-500',
+  고스트: 'bg-purple-700',
+  드래곤: 'bg-indigo-500',
+  악: 'bg-slate-500',
+  강철: 'bg-neutral-400',
+  페어리: 'bg-pink-300',
+};
