e3d330fbf095c0da8cafaa6a3e7151430fe97a7f
hyeonw8 <whgusrud223@gmail.com>
2024-07-05 01:38:13 +0900

    🩹 fix : 일부 데이터 타입 추가 및 Colors 타입 Record 적용

--- src/types/Pokemon.type.ts
+++ src/types/Pokemon.type.ts
@@ -4,29 +4,10 @@ export type Pokemon = {
   korean_name: string;
   height: number;
   weight: number;
-  sprites: { front_default: string };
+  sprites: { front_default: string; back_default: string; };
   types: { type: { name: string; korean_name: string } }[];
-  abilities: { ability: { name: string; korean_name: string } }[];
+  abilities: { ability: { name: string; korean_name: string }; is_hidden: boolean; }[];
   moves: { move: { name: string; korean_name: string } }[];
 }
 
-export type Colors = {
-  노말: string;
-  불꽃: string;
-  물: string;
-  풀: string;
-  전기: string;
-  얼음: string;
-  격투: string;
-  독: string;
-  땅: string;
-  비행: string;
-  에스퍼: string;
-  벌레: string;
-  바위: string;
-  고스트: string;
-  드래곤: string;
-  악: string;
-  강철: string;
-  페어리: string;
-};
+export type Colors = Record<string, string>;
