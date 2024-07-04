3b51f79c2a7eb6801850125ea9d0ca55986f1cea
hyeonw8 <whgusrud223@gmail.com>
2024-07-05 01:46:03 +0900

    ⚡️ performance : TanStack Query 적용하여 캐시 처리
    뒤로 가기 누를 때 마다 api 를 불러오는 것 개선

--- src/app/pokemons/_components/PokemonContainer.tsx
+++ src/app/pokemons/_components/PokemonContainer.tsx
@@ -1,43 +1,33 @@
-'use client';
+"use client";
 
-import React, { useEffect, useState } from 'react';
-import axios, { AxiosError } from 'axios';
-import PokemonList from '@/app/pokemons/_components/PokemonList';
+import React, { useEffect, useState } from "react";
+import axios, { AxiosError } from "axios";
+import PokemonList from "@/app/pokemons/_components/PokemonList";
 
-import type { Pokemon } from '@/types/Pokemon.type';
+import type { Pokemon } from "@/types/Pokemon.type";
+import { useQuery } from "@tanstack/react-query";
 
 const PokemonContainer = () => {
-  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [error, setError] = useState<null | AxiosError>(null);
+
+  const {
+    data: pokemons,
+    isPending,
+    error,
+  } = useQuery<Promise<Pokemon[]>, AxiosError, Pokemon[]>({
+    queryKey: ["pokemons"],
+    queryFn: async () => {
+      const response = await axios.get<Pokemon[]>("/api");
+      console.log(response.data);
+      return response.data;     
+    },
+  });
 
 
-  useEffect(() => {
-    const pokemonData = async () => {
-      try {
-        const response = await axios.get('/api');
-        console.log(response);
-        setPokemons(response.data);
-        setIsLoading(false);
-      } catch (error) {
-        if (error instanceof AxiosError) {
-          setError(error);
-        }
-        console.log('api오류', error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    pokemonData();
-  }, []);
-
-  if (isLoading) {
+  if (isPending) {
     return (
       <div className="h-screen flex flex-col justify-center items-center font-dalmoori">
         <div className="text-xl flex flex-col p-4 justify-center items-center gap-2 rounded-lg w-[400px]">
-          {/* <p className="font-semibold">포켓몬 도감</p> */}
-          <img src='/pokemon_logo.png' alt="포켓몬 로고" />
+          <img src="/pokemon_logo.png" alt="포켓몬 로고" />
           <p className="text-lg mt-5">--- 포켓몬 데이터 수집 중 ---</p>
         </div>
       </div>
@@ -51,8 +41,10 @@ const PokemonContainer = () => {
   }
 
   return (
-    <div><PokemonList pokemons={pokemons}/></div>
-  )
-}
+    <div>
+      <PokemonList pokemons={pokemons} />
+    </div>
+  );
+};
 
-export default PokemonContainer
+export default PokemonContainer;
